251f4f488d5512881d75ff15dcb68a746ac0b383
Bruno Girome <brunogirome@gmail.com>
2020-06-23 08:17:02 -0300

    Settigns for react navigator with stack

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
+import 'react-native-gesture-handler';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { View, StatusBar } from 'react-native';
 
-// import { Container } from './styles';
+import Routes from './routes';
 
 const src: React.FC = () => {
   return (
-    <>
+    <NavigationContainer>
       <StatusBar backgroundColor="#312e38" barStyle="light-content" />
-      <View style={{ backgroundColor: '#312e38', flex: 1 }} />
-    </>
+      <View style={{ backgroundColor: '#312e38', flex: 1 }}>
+        <Routes />
+      </View>
+    </NavigationContainer>
   );
 };
 
